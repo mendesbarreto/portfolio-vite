ce92b9a0e357205d29b5c41841caf4bf53b3f6e5
Douglas Mendes <mendes@Douglass-Mac-Studio.local>
2026-02-17 10:51:59 -0500

    chore: Added missing files for react to load properly

---
+++ src/components/common/header.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  console.log({ isMenuOpen });
+  return (
+    <header className="bg-background p-4">
+      <div className="flex max-w-1064 mx-auto px-4 justify-between items-center h-16">
+        {/* Logo area*/}
+        <div className="flex items-center font-bold">
+          <img src="/icons/ic_logo.svg" alt="Logo" className="mr-2" width={16} height={16} />
+        </div>
+        {/* Hambuger button - for mobile */}
+        <button
+          type="button"
+          className="md:hidden flex flex-col justify-center items-center w-8 h-8"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span className="w-6 h-0.5 bg-mBlue"></span>
+          <span className="w-6 h-0.5 bg-mBlue mt-1"></span>
+          <span className="w-6 h-0.5 bg-mBlue mt-1"></span>
+        </button>
+        {/* Hambuger button - for mobile */}
+        <ul className="hidden md:flex space-x-4">
+          <li>
+            <a href="/">
+              <span className="text-mTeal">#</span>home
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'node:path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
