49a2205ece0d953fdf1a1b86669083d640610c92
Douglas Mendes <mendes@Douglass-Mac-Studio.local>
2026-02-26 00:51:30 -0500

    chore: udpate window control button colors

--- src/components/common/terminal-window-controll.tsx
+++ src/components/common/terminal-window-controll.tsx
@@ -5,9 +5,9 @@ interface TerminalWindowControlProps {
 export function TerminalWindowControl({ className = '' }: TerminalWindowControlProps) {
   return (
     <div className={`md:flex hidden items-center gap-2 ${className}`}>
-      <span className="w-3 h-3 rounded-full  bg-red-500"></span>
-      <span className="w-3 h-3 rounded-full  bg-yellow-500"></span>
-      <span className="w-3 h-3 rounded-full bg-green-500"></span>
+      <span className="w-3 h-3 rounded-full   bg-mRed"></span>
+      <span className="w-3 h-3 rounded-full  bg-mYellow"></span>
+      <span className="w-3 h-3 rounded-full bg-mTeal"></span>
     </div>
   );
 }
