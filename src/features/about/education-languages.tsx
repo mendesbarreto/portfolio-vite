3f73bd756f8ff0d8c12eb657b90d114626a57415
Douglas Mendes <mendes@Douglass-Mac-Studio.local>
2026-03-03 18:21:15 -0500

    feat: Add education and languages

--- src/features/about/education-languages.tsx
+++ src/features/about/education-languages.tsx
@@ -3,20 +3,18 @@ import { SectionAbout } from './section-about';
 function EducationCard() {
   return (
     <div
-      className={`rounded-lg hover:bg-background my-2 border border-mGray/10 bg-background-light hover:border-mTeal/80`}
+      className={`rounded-lg hover:bg-background my-2 border border-mGray/20 bg-background-light hover:border-mTeal/80`}
     >
       <div className="flex items-start justify-between p-8">
         <div className="flex-1">
-          <h3 className="text-lg text-white font-bold">{'Education'}</h3>
-          <p className="text-mTeal font-semibold text-sm py-2">
-            <span>{'Education'}</span>
-            <span className="text-white">{' • '}</span>
+          <h3 className="text-2xl text-white font-bold">{'Education'}</h3>
+          <p className="text-mTeal font-semibold text-lg py-2">
             <span>{'University Anhembi Morumbi'}</span>
           </p>
-          <p className={`text-mGray text-sm`}>
+          <p className={`text-mGray text-md`}>
             {`Bachelor's Degree in Game Design and Development`}
           </p>
-          <p className="mt-2 text-xs">
+          <p className="mt-2 text-md">
             <span className="text-mTeal">{'2010'}</span>
           </p>
         </div>
@@ -25,13 +23,42 @@ function EducationCard() {
   );
 }
 
+function LanguagesCard() {
+  const languages = [
+    { language: 'English', proficiency: 'Fluent' },
+    { language: 'Portuguese', proficiency: 'Native' },
+    { language: 'French', proficiency: 'Professional' },
+  ];
+  return (
+    <div
+      className={`rounded-lg hover:bg-background my-2 border border-mGray/20 bg-background-light hover:border-mTeal/80`}
+    >
+      <div className="flex items-start justify-between p-8">
+        <div className="flex-1">
+          <h3 className="text-2xl text-white font-bold">{'Languages'}</h3>
+          {languages.map((item, index) => {
+            const id = `language-${index}`;
+            return (
+              <div key={id} className="flex justify-between items-center gap-4 mt-4">
+                <p className={`text-mGray text-lg`}>{item.language}</p>
+                <p className="text-mTeal font-semibold text-lg">
+                  <span>{item.proficiency}</span>
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
 export function EducationLanguages({ className = '' }: { className?: string }) {
   return (
     <div className={`flex flex-col w-full mb-4 ${className}`}>
       <SectionAbout title="Education & Languages" className="mb-4"></SectionAbout>
       <div className="w-full grid grid-cols-1 md:grid-cols-2 md:gap-4">
         <EducationCard />
-        <EducationCard />
+        <LanguagesCard />
       </div>
     </div>
   );
