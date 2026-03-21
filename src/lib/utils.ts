d569d3ee1b0ccabb7d4644ebaba475743cf3a154
Douglas Mendes <mendes@Douglass-Mac-Studio.local>
2026-03-21 17:03:44 -0400

    chore: Add mixpanel to website

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const isLocalhost =
+  window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { QueryClientProvider } from '@tanstack/react-query';
+import mixpanel from 'mixpanel-browser';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { queryClient } from './queryClient';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+import { isLocalhost } from './lib/utils';
+
+if (!isLocalhost) {
+  mixpanel.init('d0d89609d7c6db05b69ee7d96c6df250', {
+    autocapture: true,
+    track_pageview: true,
+    record_sessions_percent: 100, //records 100% of all sessions
+    record_heatmap_data: true,
+  });
+}
 
 const router = createRouter({ routeTree });
 const root = document.getElementById('root');
